1e53f41a90ffdba9f5248f1f2d0554e0ee6d46c8
Tien <tien.dzung.upwork@gmail.com>
2022-01-03 18:01:00 +0700

    Implement the click logic from Login -> OTP -> Main

--- app/src/main/java/com/nearlabs/nftmarketplace/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/nearlabs/nftmarketplace/ui/auth/LoginFragment.kt
@@ -5,9 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.nearlabs.nftmarketplace.R
+import com.nearlabs.nftmarketplace.common.extensions.viewBinding
+import com.nearlabs.nftmarketplace.databinding.FragmentLoginBinding
 import com.nearlabs.nftmarketplace.ui.base.BaseFragment
 
 class LoginFragment : BaseFragment(R.layout.fragment_login) {
 
+    private val binding by viewBinding(FragmentLoginBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.toOtp)
+        }
+    }
 }
