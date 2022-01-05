8c9e32679152a8e358aa658eda3a7de21fa05e33
ahmad-naeem-confiz <ahmad.naeem@confiz.com>
2022-01-05 12:27:37 +0500

    sdk integration

--- app/src/main/java/com/nearlabs/nftmarketplace/NftApplication.kt
+++ app/src/main/java/com/nearlabs/nftmarketplace/NftApplication.kt
@@ -1,8 +1,16 @@
 package com.nearlabs.nftmarketplace
 
 import android.app.Application
+import com.appsflyer.AppsFlyerLib
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NftApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppsFlyerLib.getInstance().setDebugLog(true)
+        AppsFlyerLib.getInstance().init(BuildConfig.AppsFlyer_Dev_Key, null, this)
+        AppsFlyerLib.getInstance().start(this,BuildConfig.AppsFlyer_Dev_Key)
+    }
 }
