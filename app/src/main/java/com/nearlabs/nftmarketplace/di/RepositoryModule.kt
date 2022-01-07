f2c66ac25d3fa43426e6fb5be95955064566eaea
Tien <tien.dzung.upwork@gmail.com>
2022-01-07 14:17:36 +0700

    Implement local contact loading

--- app/src/main/java/com/nearlabs/nftmarketplace/di/RepositoryModule.kt
+++ app/src/main/java/com/nearlabs/nftmarketplace/di/RepositoryModule.kt
@@ -1,5 +1,8 @@
 package com.nearlabs.nftmarketplace.di
 
+import android.content.Context
+import com.nearlabs.nftmarketplace.data.localcontact.ContactSource
+import com.nearlabs.nftmarketplace.data.localcontact.LocalContact
 import com.nearlabs.nftmarketplace.data.networks.*
 import com.nearlabs.nftmarketplace.data.preference.SharePrefs
 import com.nearlabs.nftmarketplace.repository.Repository
@@ -7,6 +10,7 @@ import com.nearlabs.nftmarketplace.repository.SettingsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -23,7 +27,8 @@ class RepositoryModule {
         contactApi: ContactApi,
         userApi: UserApi,
         loginApi: LoginApi,
-        sharePrefs: SharePrefs
+        sharePrefs: SharePrefs,
+        localContact: ContactSource
     ) =
         Repository(
             api,
@@ -32,11 +37,17 @@ class RepositoryModule {
             nftApi,
             userApi,
             loginApi,
-            sharePrefs
+            sharePrefs,
+            localContact
         )
 
     @Provides
     @Singleton
     fun provideSettingsRepository(api: Api, sharePrefs: SharePrefs) =
         SettingsRepository(api, sharePrefs)
+
+    @Provides
+    @Singleton
+    fun providerContactSource(@ApplicationContext context: Context): ContactSource =
+        LocalContact(context)
 }
